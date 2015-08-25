2f5128f2b944e02916a2a3b2257cbc95f9a6baac
limbo <lingbo.wang@dianping.com>
2015-08-25 20:53:38 +0800

    Add redis support.

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var Mock = require('mockjs');
+var redis = require('redis');
 var router = express.Router();
 var data = require('../data/data');
+var client = redis.createClient();
 
-/* GET users listing. */
+client.on('error', function(e) {
+  console.log('ERROR: ' + e);
+})
+
 router.get('/', function(req, res, next) {
-  res.send('This is api for test');
+  res.send('This is index page.');
 });
 
-router.get('/fun/ktv/precreateorder', function(req, res, next) {
-  for (var i in data) {
-    if (data[i].url == '/fun/ktv/precreateorder') {
-      res.jsonp(Mock.mock(data[i].tpl));
-      break;
-    }
-  }
+router.get(/^(.*)$/, function(req, res, next) {
+  var path = req.params[0];
+  var query = req.query;
+
+  client.get(path, function(e, data) {
+    res.json({
+      path: path,
+      data: data
+    });
+  })
+});
+
+router.post('/create', function(req, res, next) {
+  var query = req.body;
+
+  client.set(query.path, query.data, function(e, r) {
+    res.json(r);
+  })
 });
 
 module.exports = router;
